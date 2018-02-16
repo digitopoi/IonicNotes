41095d6726534abcc0de1b7a182faa9cf64697b5
digitopoi <hubo85@gmail.com>
2018-02-16 12:46:21 -0600

    Add persistence with ionic storage

--- src/providers/note-service/note-service.ts
+++ src/providers/note-service/note-service.ts
@@ -1,20 +1,28 @@
+import { Note } from './../../_models/note.model';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class NoteService {
 
-  private notes: { title: string }[] = [];
+  private notes: Note[] = [];
 
-  constructor() {
+  constructor(public storage: Storage) {
   }
 
-  saveNote(note: { title: string }) {
+  saveNote(note: Note) {
+    note.createDate = Date.now(); 
     this.notes.push(note);
+    this.storage.set('notes', this.notes);
   }
 
   getAllNotes() {
-    return [...this.notes];
+    return this.storage.get('notes').then(
+      (notes) => {
+        this.notes = notes == null ? [] : notes;
+        return [...this.notes];
+      }
+    )
   }
 
 }
